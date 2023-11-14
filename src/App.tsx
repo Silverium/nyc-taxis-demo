660a08d2d169f8c1ea0e0895565a18bb84c5cc29
Silverium <soldeplatadeveloper@gmail.com>
2023-11-14 12:49:05 +0100

    Refactor App component with MUI components

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import TableFilter from "./components/TableFilter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
-import { Link, ListItemText } from "@mui/material";
+import { Box, Link, ListItemText, Typography } from "@mui/material";
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <h1>Coding exercise</h1>
-        <h2>Tech stack used</h2>
+      <Box className="App">
+        <Typography variant="h2" component="h1">
+          Coding exercise
+        </Typography>
+        <Typography variant="h3" component="h1">
+          Tech stack used
+        </Typography>
+
         <List>
           <ListItem alignItems="center">
             <ListItemText sx={{ textAlign: "center" }}>
@@ -37,15 +42,17 @@ export default function App() {
             </ListItemText>
           </ListItem>
         </List>
-        <h2>Instructions</h2>
-        <p>
+        <Typography variant="h3" component="h1">
+          Instructions
+        </Typography>
+        <Typography>
           The goal of this exercise is to create a data grid that displays data
           from the JSONPlaceholder API. The data grid should be able to filter
           by title, to be able to edit fields, and to highlight (with italic) the title of the rows where the
           user ID is a prime number.
-        </p>
+        </Typography>
         <TableFilter />
-      </div>
+      </Box>
     </QueryClientProvider>
   );
 }
