c75447ce944cc1f540ed01c7fdc71a222221b5e7
CodeSandbox <support@codesandbox.io>
2023-11-14 09:42:20 +0000

    Sandbox snapshot commit

---
+++ src/components/EditTextArea.tsx
@@ -0,0 +1,58 @@
+export default function EditTextarea(props: GridRenderEditCellParams<any, string>) {
+    const { id, field, value, colDef, hasFocus } = props;
+    const [valueState, setValueState] = React.useState(value);
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>();
+    const [inputRef, setInputRef] = React.useState<HTMLInputElement | null>(null);
+    const apiRef = useGridApiContext();
+  
+    React.useLayoutEffect(() => {
+      if (hasFocus && inputRef) {
+        inputRef.focus();
+      }
+    }, [hasFocus, inputRef]);
+  
+    const handleRef = React.useCallback((el: HTMLElement | null) => {
+      setAnchorEl(el);
+    }, []);
+  
+    const handleChange = React.useCallback<NonNullable<InputBaseProps['onChange']>>(
+      (event) => {
+        const newValue = event.target.value;
+        setValueState(newValue);
+        apiRef.current.setEditCellValue(
+          { id, field, value: newValue, debounceMs: 200 },
+          event,
+        );
+      },
+      [apiRef, field, id],
+    );
+  
+    return (
+      <div style={{ position: 'relative', alignSelf: 'flex-start' }}>
+        <div
+          ref={handleRef}
+          style={{
+            height: 1,
+            width: colDef.computedWidth,
+            display: 'block',
+            position: 'absolute',
+            top: 0,
+          }}
+        />
+        {anchorEl && (
+          <Popper open anchorEl={anchorEl} placement="bottom-start">
+            <Paper elevation={1} sx={{ p: 1, minWidth: colDef.computedWidth }}>
+              <InputBase
+                multiline
+                rows={4}
+                value={valueState}
+                sx={{ textarea: { resize: 'both' }, width: '100%' }}
+                onChange={handleChange}
+                inputRef={(ref) => setInputRef(ref)}
+              />
+            </Paper>
+          </Popper>
+        )}
+      </div>
+    );
+  }
--- src/components/TableFilter.tsx
+++ src/components/TableFilter.tsx
@@ -4,21 +4,82 @@ import {
   DataGrid,
   GridToolbar,
   GridColDef,
-  GridRowParams
+  GridRowParams,
+  GridRenderEditCellParams,
+  useGridApiContext,
 } from "@mui/x-data-grid";
 import { useQuery } from "@tanstack/react-query";
+import { InputBase, InputBaseProps, Paper, Popper } from "@mui/material";
 
 type MockItem = {
   id: number;
   userId: number;
   title: string;
   body: string;
+};
+function EditTextarea(props: GridRenderEditCellParams<any, string>) {
+  const { id, field, value, colDef, hasFocus } = props;
+  const [valueState, setValueState] = React.useState(value);
+  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>();
+  const [inputRef, setInputRef] = React.useState<HTMLInputElement | null>(null);
+  const apiRef = useGridApiContext();
+
+  React.useLayoutEffect(() => {
+    if (hasFocus && inputRef) {
+      inputRef.focus();
+    }
+  }, [hasFocus, inputRef]);
+
+  const handleRef = React.useCallback((el: HTMLElement | null) => {
+    setAnchorEl(el);
+  }, []);
+
+  const handleChange = React.useCallback<NonNullable<InputBaseProps['onChange']>>(
+    (event) => {
+      const newValue = event.target.value;
+      setValueState(newValue);
+      apiRef.current.setEditCellValue(
+        { id, field, value: newValue, debounceMs: 200 },
+        event,
+      );
+    },
+    [apiRef, field, id],
+  );
+
+  return (
+    <div style={{ position: 'relative', alignSelf: 'flex-start' }}>
+      <div
+        ref={handleRef}
+        style={{
+          height: 1,
+          width: colDef.computedWidth,
+          display: 'block',
+          position: 'absolute',
+          top: 0,
+        }}
+      />
+      {anchorEl && (
+        <Popper open anchorEl={anchorEl} placement="bottom-start">
+          <Paper elevation={1} sx={{ p: 1, minWidth: colDef.computedWidth }}>
+            <InputBase
+              multiline
+              rows={4}
+              value={valueState}
+              sx={{ textarea: { resize: 'both' }, width: '100%' }}
+              onChange={handleChange}
+              inputRef={(ref) => setInputRef(ref)}
+            />
+          </Paper>
+        </Popper>
+      )}
+    </div>
+  );
 }
 const exerciseCols: GridColDef[] = [
-  // { field: "id", hidden: true },
-  { field: "title", width: 400, headerName: "Title" },
+  { field: "id", hideable: true },
+  { field: "title", width: 400, headerName: "Title", editable: true, renderEditCell: (params) => <EditTextarea {...params} />, type: "string" },
   { field: "userId", headerName: "User ID", width: 100 },
-  { field: "body", width: 400, headerName: "Text Body" }
+  { field: "body", width: 400, headerName: "Text Body" },
 ];
 function generatePrimeNumbersObject(n: number): { [key: number]: boolean } {
   const primes: { [key: number]: boolean } = {};
@@ -40,14 +101,17 @@ function generatePrimeNumbersObject(n: number): { [key: number]: boolean } {
 
   return primes;
 }
+
 export default function QuickFilteringGrid() {
-  const [primeNumbers, setPrimeNumbers] = React.useState<{ [key: number]: boolean }>({ 2: true });
+  const [primeNumbers, setPrimeNumbers] = React.useState<{
+    [key: number]: boolean;
+  }>({ 2: true });
   const query = useQuery<MockItem[]>({
     queryKey: ["posts"],
     queryFn: () =>
       fetch("https://jsonplaceholder.typicode.com/posts").then((response) =>
-        response.json()
-      )
+        response.json(),
+      ),
   });
   React.useEffect(() => {
     if (query.data) {
@@ -62,12 +126,12 @@ export default function QuickFilteringGrid() {
 
   const getRowClassName = React.useCallback(
     (params: GridRowParams) => {
-      if(primeNumbers[params.row.userId]){
-        return "italic"
+      if (primeNumbers[params.row.userId]) {
+        return "italic";
       }
-     return ""
+      return "";
     },
-    [primeNumbers]
+    [primeNumbers],
   );
 
   return (
@@ -82,9 +146,10 @@ export default function QuickFilteringGrid() {
         slots={{ toolbar: GridToolbar }}
         slotProps={{
           toolbar: {
-            showQuickFilter: true
-          }
+            showQuickFilter: true,
+          },
         }}
+        onCellEditStop={(params, event)=> console.log("we could save here",{params, event})}
       />
     </Box>
   );
