d1829c5c371c1141d075d1ad1b1c4018b9c7735f
Silverium <soldeplatadeveloper@gmail.com>
2023-11-14 12:34:22 +0100

    Add MUI List and ListItem components for better UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import "./styles.css";
 import TableFilter from "./components/TableFilter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 const queryClient = new QueryClient();
 
 export default function App() {
@@ -11,30 +12,30 @@ export default function App() {
       <div className="App">
         <h1>Coding exercise</h1>
         <h2>Tech stack used</h2>
-        <ul>
-          <li>
+        <List>
+          <ListItem alignItems="center">
             <a href="https://mui.com/components/data-grid/">
               MUI Data Grid
             </a>
-          </li>
-          <li>
+          </ListItem >
+          <ListItem alignItems="center">
             <a href="https://react-query.tanstack.com/">
               React Query
             </a>
-          </li>
-          <li>
+          </ListItem>
+          <ListItem alignItems="center">
             <a href="https://jsonplaceholder.typicode.com/">
               JSONPlaceholder
             </a>
-          </li>
-        </ul>
+          </ListItem>
+        </List>
         <h2>Instructions</h2>
         <p>
           The goal of this exercise is to create a data grid that displays data
           from the JSONPlaceholder API. The data grid should be able to filter
           by title, to be able to edit fields, and to highlight (with italic) the title of the rows where the
           user ID is a prime number.
-          </p>
+        </p>
         <TableFilter />
       </div>
     </QueryClientProvider>
