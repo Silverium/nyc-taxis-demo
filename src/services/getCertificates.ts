4d3b9ba76c12da79614abb8e820a2a7c07070230
Silverium <soldeplatadeveloper@gmail.com>
2023-11-23 10:00:47 +0100

    Refactor getCertificates function to use async/await

--- src/services/getCertificates.ts
+++ src/services/getCertificates.ts
@@ -1,16 +1,20 @@
 import { CertificatesResult } from "@/types/CertificatesResponse";
 
-export const getCertificates = () => fetch("https://demo.api.agreena.com/api/public/carbon_registry/v1/certificates?includeMeta=true&page=1&limit=10", {
-  headers: {
-    "API-ACCESS-TOKEN": "Commoditrader-React-FE-Farmer"
+export const getCertificates = async () => {
+  try {
+    const response = await fetch("https://demo.api.agreena.com/api/public/carbon_registry/v1/certificates?includeMeta=true&page=1&limit=10", {
+      headers: {
+        "API-ACCESS-TOKEN": "Commoditrader-React-FE-Farmer"
+      }
+    });
+    const { result: { data } }: CertificatesResult = await response.json();
+    return data.flatMap((certificate) => {
+      return certificate.combinedCertificates || certificate;
+    });
+  } catch (err) {
+    console.error(err);
+    return [];
   }
-}).then((response) => response.json()
-).then(({ result: { data } }: CertificatesResult) => data.flatMap((certificate) => {
-  return certificate.combinedCertificates || certificate;
-
-})).catch((err) => {
-  console.error(err);
-  return [];
-});
+};
 
 export default getCertificates;
