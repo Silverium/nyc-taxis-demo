700a61247c726f6e7b0547854bc66ceac0316af2
Silverium <soldeplatadeveloper@gmail.com>
2023-11-25 12:14:54 +0100

    Add GitHub link to NavigationHeader component

--- src/components/NavigationHeader.tsx
+++ src/components/NavigationHeader.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const NavigationHeader: React.FC = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6">NYC Taxi Demo</Typography>
-        <Button component={Link} to="/" color="inherit">Home</Button>
-        <Button component={Link} to="/taxi-trips" color="inherit">Taxi trips</Button>
+      <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Box display="flex" flexDirection={"row"}>
+          <Typography variant="h6">NYC Taxi Demo</Typography>
+          <Button component={Link} to="/" color="inherit">Home</Button>
+          <Button component={Link} to="/taxi-trips" color="inherit">Taxi trips</Button>
+        </Box>
+        <Button component={Link} to="https://github.com/Silverium/nyc-taxis-demo" color="inherit">
+          <GitHubIcon />
+        </Button>
       </Toolbar>
     </AppBar>
   );
